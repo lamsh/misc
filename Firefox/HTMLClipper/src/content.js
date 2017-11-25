a426a231a008aa1b2b803955f18460e8b0b358ab
SENOO Ken <mslamsh20131029@outlook.jp>
2017-11-25 23:00:37 +0900

    Release first version 1.0.0

--- Firefox/HTMLClipper/src/content.js
+++ Firefox/HTMLClipper/src/content.js
@@ -12,7 +12,7 @@ function copyAsHTML(tab){
   // Create URL DOM
   var field = document.createElement("a");
   field.setAttribute("href", tab.url);
-  field.innerHTML = tab.title;
+  field.innerText = tab.title;
   document.body.appendChild(field);
 
   var range = document.createRange();
